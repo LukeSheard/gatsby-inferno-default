558b90ecc066f472dad552a6f3ec7a688df8230c
Luke Sheard <me@lukesheard.com>
2016-11-20 22:23:49 +0000

    VNode render bug

--- html.js
+++ html.js
@@ -1,32 +1,29 @@
-import Component from 'inferno-component';
 import Inferno from 'inferno';
 import { prefixLink } from 'gatsby-helpers';
 
 const BUILD_TIME = new Date().getTime()
 
-export default class HTML extends Component {
-  render() {
-    let css
-    if (process.env.NODE_ENV === 'production') {
-      css = <style dangerouslySetInnerHTML={{ __html: require('!raw!./public/styles.css') }} />
-    }
-
-    return (
-      <html lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          {css}
-        </head>
-        <body>
-          <div id="react-mount" dangerouslySetInnerHTML={{ __html: this.props.body }} />
-          <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
-        </body>
-      </html>
-    )
+module.exports = function(props) {
+  let css
+  if (process.env.NODE_ENV === 'production') {
+    css = <style dangerouslySetInnerHTML={{ __html: require('!raw!./public/styles.css') }} />
   }
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+        {css}
+      </head>
+      <body>
+        <div id="react-mount" dangerouslySetInnerHTML={{ __html: props.body }} />
+        <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
+      </body>
+    </html>
+  )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,10 @@
 import Component from 'inferno-component';
 import Inferno from 'inferno';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    )
-  }
+module.exports = function() {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
 }
